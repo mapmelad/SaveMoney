8ac1e112b74d70326cebfb249cbfa5a37f9d9dcb
mapmelad <AbSemyon@gmail.com>
2018-02-17 14:10:15 +0300

    edit settingsView

--- SaveMoney/Extensions.swift
+++ SaveMoney/Extensions.swift
@@ -23,3 +23,22 @@ extension String {
         return (self as NSString).size(withAttributes: [NSAttributedStringKey.font: font])
     }
 }
+
+func getRussianMonth(_ indexMonth: Int) -> String {
+    let sDict: [Int: String] = [
+        1: "Января",
+        2: "Февраля",
+        3: "Марта",
+        4: "Апреля",
+        5: "Мая",
+        6: "Июня",
+        7: "Июля",
+        8: "Августа",
+        9: "Сентября",
+        10: "Октября",
+        11: "Ноября",
+        12: "Декабря"
+        ]
+    
+    return sDict[indexMonth] ?? ""
+}
--- SaveMoney/ViewController/ViewController.swift
+++ SaveMoney/ViewController/ViewController.swift
@@ -6,22 +6,101 @@
 //  Copyright © 2018 Semyon. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var spendAmountTextField: UITextField!
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
+    @IBOutlet var button5: UIButton!
+    @IBOutlet var button6: UIButton!
+    @IBOutlet var button7: UIButton!
+    @IBOutlet var button8: UIButton!
+    @IBOutlet var button9: UIButton!
+    @IBOutlet var deleteButton: UIButton!
+    
     let category = ["Транспорт 🚎", "Бары 🍻", "Кафе 🍟", "Одежда 👟", "Сотовая связь 📱"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindTipsAmount()
+        
+        for i in 1..<11 {
+            if let v = view.viewWithTag(i) as? UIButton {
+                v.rx.tap.subscribe(onNext: { [unowned self] _ in
+                    self.onButtonTap(i - 1)
+                })
+            }
+        }
+        
+        deleteButton.rx.tap.subscribe(onNext: { [unowned self] _ in
+            self.spendAmountTextField.text?.removeLast()
+        })
+        
     }
+    
+    private func onButtonTap(_ idx: Int) {
+        log.debug("did tap - \(idx)")
+        let s = String(idx)
+        let h = spendAmountTextField.hasText
+        if !h {
+            spendAmountTextField.text?.append(s)
+        } else {
+            let text = spendAmountTextField.text!
+            let tidx = text.index(text.endIndex, offsetBy: -2)
+            spendAmountTextField.text?.insert(Character(s), at: tidx)
+        }
+        moveCaret()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func bindTipsAmount() {
+        let ft = spendAmountTextField.rx.text.filter { $0 != nil && !($0 ?? "").isEmpty }.map { $0 ?? "" }
+        ft.subscribe { s in
+            self.updateTipsTextField(s.element!)
+            // self.spendAmountTextField.text = ""
+        }
+        
+    }
+    
+    func updateTipsTextField(_ currentText: String) {
+        if currentText.count == 2 && currentText == " ₽" {
+            spendAmountTextField.text = nil
+        } else {
+            appendRubleSymbol(currentText)
+            moveCaret()
+        }
+    }
+    
+    private func appendRubleSymbol(_ currentText: String) {
+        
+        let t = spendAmountTextField.text ?? ""
+        print(t)
+        if t.last != "₽" { spendAmountTextField.text?.append(" ₽") }
+    }
+    
+    private func moveCaret() {
+        guard let newPosition = spendAmountTextField.position(from: spendAmountTextField.endOfDocument, offset: -2) else { return }
+        let newRange = spendAmountTextField.textRange(from: newPosition, to: newPosition)
+        spendAmountTextField.selectedTextRange = newRange
+    }
+    
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: self.category[indexPath.row].size(OfFont: UIFont.systemFont(ofSize: 17.0)).width + 15.0, height: 30)
+        return CGSize(width: category[indexPath.row].size(OfFont: UIFont.systemFont(ofSize: 17.0)).width + 15.0, height: 30)
     }
     
     public func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -29,7 +108,7 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDe
     }
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.category.count
+        return category.count
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
